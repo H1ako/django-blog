a28e3fb6ef755fccd59ab51f28bc3c50de2a6468
H1ako <nikita-sobolev-wd@yandex.ru>
2022-10-06 09:11:11 +0400

    fixed paginator bug

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.db.models.functions import Length
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 from rest_framework import views, permissions, status
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -16,19 +16,27 @@ class PostsView(views.APIView):
 
     def get(self, req: Request) -> Response:
         status_code = status.HTTP_200_OK
+        error_details = ''
         page = req.GET.get('page', 1)
         user = req.user
 
         posts = self.get_posts(user).order_by(Length('views'))
         paginator = Paginator(posts, 10)
-        currentPage = paginator.page(page)
-        postsData = PostSerializer(currentPage.object_list, many=True, context={
-            'request': req
-        }).data
+        postsData = []
+
+        try:
+            currentPage = paginator.page(page)
+            postsData = PostSerializer(currentPage.object_list, many=True, context={
+                'request': req
+            }).data
+        except EmptyPage:
+            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+            error_details = 'No such page'
 
         data = {
             'posts': postsData,
-            'lastPage': currentPage.end_index()
+            'lastPage': paginator.page_range[-1],
+            'errorDetails': error_details
         }
 
         return Response(data, status=status_code)
